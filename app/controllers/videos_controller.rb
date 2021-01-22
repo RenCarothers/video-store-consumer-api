e69504b3354502dd7d22d49264425e6803801357
Blaine <b.atsbeha@gmail.com>
2021-01-21 17:38:05 -0800

    created add-vid create to routes and method to video controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -21,7 +21,32 @@ class VideosController < ApplicationController
       )
   end
 
+  def create
+    video = Video.new(video_params)
+    if Video.find_by(external_id: params[:external_id]).nil?
+      if video.save
+        render json: video.as_json(only: [:id]), status: :created
+        return
+      else
+        render json: { errors: video.errors.messages }, status: :bad_request
+        return
+      end
+    end
+    # this is where we stopped - how do we create a video based on search results
+    # video = Video.new(params)
+    # do we need to create a starting inventory or is it just one??  yes, no, maybe ... functional reqs contradict readMe/
+    # instructions mention how to handle if a video has already been added ->
+    # I think we account for this with the external id -- if a video exists with the same ext. id then don't create
+    # else, we do video.save
+    # video = VideoWrapper.search(params)
+    # ap "#{video.title} Added to the library!"
+  end
+
   private
+
+  def video_params
+    return params.permit(:title, :overview, :release_date, :image_url, :external_id, )
+  end
 
   def require_video
     @video = Video.find_by(title: params[:title])
